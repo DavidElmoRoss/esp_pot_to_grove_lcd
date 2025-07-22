96ae70073ce4afb8ca9f3f0056aa59dfb0900aee
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2025-07-22 15:48:48 -0400

    Update main.cpp
    changed the buff size to 16, and changed screen formatting from %d to %4d

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,20 @@
                the Analog Value & Voltage on the SERIAL MONITOR and GROVE LCD
 */
 
-
 #include <Arduino.h>                // include arduino.h
 
 #include <Wire.h>                   // include Wire.h library
 #include "rgb_lcd.h"                // include rgb_lcd library
 #define PIN_ANALOG_IN 34            // Pin 34 is the A/D being used
                                     // other a2d pins are 35,36, 39
-char buff[10];                      // char buff for convert float to string
+char buff[16];                      // char buff for convert float to string
 short adcVal;                       // declare adcVal as a short
 double voltage;                     // declare voltage as a double
 rgb_lcd lcd;                        // make lcd an instance of rgb_lcd 
 
 void setup()
 {
-    Serial.begin(115200);           // platformio.ini has monitor_speed = 115200
-
-
-                                  
-  
+  Serial.begin(115200);           // platformio.ini has monitor_speed = 115200                                    
   Wire.begin();                     // initialize WIRE system
   lcd.begin(16,2);                  // start up the LCD
   lcd.display();                    // turn on LCD display
@@ -45,7 +40,7 @@ void loop()
     adcVal = analogRead(PIN_ANALOG_IN);   // adcVal gets value from 0-4095
     voltage = adcVal / 4096.0 * 3.333;    // voltages gets value from 0-3.33
     printf("\e[11;35H");                  // position cursor at (11,35)
-    printf("%d\t\t%4.2f",adcVal,voltage); // print adcVal and voltage there
+    printf("%4d\t\t%4.2f",adcVal,voltage); // print adcVal and voltage there
     sprintf(buff,"%4d   %4.2f",           // convert adVal, voltage to 
             adcVal,voltage);              // string array and put in buff
     lcd.setCursor(2,1);                   // position cursor at col 2 and row 1
